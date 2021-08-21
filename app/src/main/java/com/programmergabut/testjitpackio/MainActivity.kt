7ee2398d48ad5f2dc711181c69f853ce1b0130f7
jiwo <mdf.jiwo@gmail.com>
2021-08-21 22:34:48 +0700

    15 commit

--- app/src/main/java/com/programmergabut/testjitpackio/MainActivity.kt
+++ app/src/main/java/com/programmergabut/testjitpackio/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         /* val url = "http://www.google.com?test=mantapdjiwa"
         val i = Intent(Intent.ACTION_VIEW)
         i.data = Uri.parse(url)
